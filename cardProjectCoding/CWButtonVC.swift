0666b292cbcf0023a917c1d023ba997e898a2d41
i-derekGaleas <64647310+DHK2001@users.noreply.github.com>
2024-09-16 12:55:02 -0600

    adding card code project

---
+++ cardProjectCoding/CWButtonVC.swift
@@ -0,0 +1,16 @@
+//
+//  CWButtonVC.swift
+//  cardProjectCoding
+//
+//  Created by Derek H. Galeas on 20/5/24.
+//
+
+import UIKit
+
+class CWButtonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
